190c90c32329bd429d92313af5f041aa77dfe2c3
Steven Troxler <steven.troxler@berkeleyanalytics.com>
2014-11-07 14:18:42 -0800

    Added an extra missed dependency.

--- ubuntu/install_RStudio.sh
+++ ubuntu/install_RStudio.sh
@@ -3,8 +3,11 @@
 # server version, which lets you connect via the web. But if you are
 # using X forwarding to work with the server, this is the way to go.
 
-# THIS FILE PROBABLY NEEDS TO BE UPDATED WITH A NEW URL
-sudo apt-get install gdebi-core
-sudo apt-get install libapparmor1
+# THIS FILE PROBABLY NEEDS TO BE UPDATED WITH A NEW URL AS RSTUDIO
+# GETS UPDATED. JUST GO TO THE DOWNLOADS PAGE AND COPY THE LINK.
+
+sudo apt-get install -y gdebi-core
+sudo apt-get install -y libapparmor1
+sudo apt-get install -y libgstreamer-plugins-base0.10-0
 wget http://download1.rstudio.org/rstudio-0.98.1091-amd64.deb
 sudo gdebi rstudio-0.98.1091-amd64.deb
